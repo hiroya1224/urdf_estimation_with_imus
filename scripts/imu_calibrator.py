4f16f5fb5121ec956989f3bf8ad007d7c755a7f7
Hiroya Sato <h-sato@jsk.imi.i.u-tokyo.ac.jp>
2024-10-13 10:29:29 +0900

    reduce print rate in calib

--- scripts/imu_calibrator.py
+++ scripts/imu_calibrator.py
@@ -102,7 +102,8 @@ class ImuCalibratorROS:
             self.update_list(container.ddomg_list_for_cov, ddomg_raw)
 
             counter = [e is None for e in container.omg_list_for_cov].count(False)
-            rospy.logwarn("Do not move your robot: {} / {}".format(counter, container.omg_size))
+            if counter % 50 == 0:
+                rospy.logwarn("Do not move your robot: {} / {}".format(counter, container.omg_size))
 
             if not container.omg_list_for_cov[0] is None:
                 omega_dataset = np.vstack(container.omg_list_for_cov).T
@@ -125,7 +126,9 @@ class ImuCalibratorROS:
             self.update_list(container.acc_list, acc_raw)
 
             counter = [e is None for e in acc_list].count(False)
-            rospy.logwarn("Rotate your robot slowly: {} / {}".format(counter, container.acc_size))
+            
+            if counter % 50 == 0:
+                rospy.logwarn("Rotate your robot slowly: {} / {}".format(counter, container.acc_size))
 
             if not acc_list[0] is None:
                 acc_dataset = np.vstack(acc_list).T
